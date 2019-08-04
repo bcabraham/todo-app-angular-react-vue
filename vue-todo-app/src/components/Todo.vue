882915aa7fad32cf64e9b0523605ae92448f4bbd
bcabraham <abraham.bryan@gmail.com>
2019-08-03 18:08:04 -0600

    fixed spacing issue

--- vue-todo-app/src/components/Todo.vue
+++ vue-todo-app/src/components/Todo.vue
@@ -1,15 +1,15 @@
 <template>
   <b-list-group-item id="Todo">
     <b-row>
-      <b-col md="10" :class="{isDone: this.todo.completed}">{{this.todo.text}}</b-col>
-      <b-col md="1" class="pb-2">
+      <b-col md="8" :class="{isDone: this.todo.completed}">{{this.todo.text}}</b-col>
+      <b-col md="2" class="pb-2">
         <b-button
           size="md"
           variant="outline-primary"
           @click="toggleDone"
         >{{ this.todo.completed ? "unDone": "Done" }}</b-button>
       </b-col>
-      <b-col md="1" class="pb-2">
+      <b-col md="2" class="pb-2">
         <b-button size="md" variant="outline-primary" @click="$emit('remove-todo')">Delete</b-button>
       </b-col>
     </b-row>
@@ -32,9 +32,5 @@ export default {
 <style>
 .isDone {
   text-decoration: line-through;
-}
-
-b-list-group-item {
-  display: inline-block;
 }
 </style>
